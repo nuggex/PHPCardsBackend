005a6b6366a6d575e2b24ce83cd16a6de630ad8a
Nugge Boman <nuggex@gmail.com>
2024-08-05 23:32:32 +0300

    Swoole test

--- autoloader.php
+++ autoloader.php
@@ -2,6 +2,8 @@
 
 define('ROOT_DIR', dirname(__FILE__));
 const WEBROOT_DIR = ROOT_DIR . '/webroot/';
+
+require_once 'vendor/autoload.php';
 /**
  * Autoload classes from /classes/ directory. If namespaces are used then create matching subdirectory structure.
  */
@@ -10,13 +12,6 @@ function AutoLoad($className): void
     $file = ROOT_DIR . '/classes/' . str_replace("\\", DIRECTORY_SEPARATOR, $className) . '.php';
     if (is_file($file)) {
         require_once($file);
-    } else {
-        $file = ROOT_DIR . '/include/' . $className . '.php';
-        if (is_file($file)) {
-            require_once($file);
-        } else {
-            echo "$file not found\n";
-        }
     }
 }
 
