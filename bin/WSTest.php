7019e5941554f0a9dfb7cf28ed872bf5fbb4f4f0
Markus Kankkonen <markus.kankkonen@protonmail.com>
2024-08-22 18:36:30 +0300

    Created basic websocket logic

--- bin/WSTest.php
+++ bin/WSTest.php
@@ -6,30 +6,82 @@ $server = new OpenSwoole\Websocket\Server("127.0.0.1", 9502);
 
 $players = [];
 
-$server->on('open', function($server, $req) {
-    global $players;
-    echo "connection open: {$req->fd}\n";
-    foreach ($server->connections as $fd){
-        $connectionInfo = $server->getClientInfo($fd);
-        echo "FD: $fd, IP: {$connectionInfo['remote_ip']}, Port: {$connectionInfo['remote_port']}\n";
+$server->on('open', function ($server, $req) {
+  global $players;
+  echo "connection open: {$req->fd}\n";
+  foreach ($server->connections as $fd) {
+    $connectionInfo = $server->getClientInfo($fd);
+    echo "FD: $fd, IP: {$connectionInfo['remote_ip']}, Port: {$connectionInfo['remote_port']}\n";
+  }
+});
+
+$server->on('message', function ($server, $frame) {
+  echo "received message: {$frame->data}\n";
+  $data = json_decode($frame->data);
+
+  if ($data->msg === "START") {
+
+    foreach ($server->connections as $playerId) {
+
+      $res = [
+        "type" => "INIT_GAME",
+        "gameType" => "PASKAHOUSU",
+        "deck" => [
+          // A Deck of hidden cards
+        ],
+        "cardsOnTable" => [
+          // A Deck of visible cards
+        ],
+        "otherPlayers" => [
+          // This should maybe be a list of Players instances
+          [
+            "playerId" => 42,
+            "cardsInHand" => ["?", "?", "?"]
+          ]
+        ],
+        "currentPlayer" =>
+        // This should maybe be a instance of a Player class 
+        [
+          "playerId" => $playerId,
+          "cardsInHand" => ["A1", "H7", "D10"]
+        ]
+
+      ];
+      $server->push($playerId, json_encode($res));
     }
+  }
+  if ($data->msg === "END_ROUND") {
+    $playerWhoseTurnItIs = $data->currentPlayerId;
+    $selectedCards = $data->selectedCards;
+
+    // TODO: Do some game logic with the cards the player selected so we get the new state of the game
+
+    // TODO: if a player won the game do some game ending thing  
+
+    foreach ($server->connections as $playerId) {
+      // TODO: Send the new deck state, cardsOnTable, otherPlayers and currentPlayer 
+      // currentPlayer state data based on the playerId 
+
+      $res = [
+        "type" => "NEXT_ROUND",
+        "deck" => [],
+        "cardsOnTable" => [],
+        "otherPlayers" => [
+          []
+        ],
+        "currentPlayer" =>
+        [
+          "playerId" => $playerId,
+          "cardsInHand" => ["A1", "H7", "D10"]
+        ]
+      ];
+      $server->push($playerId, json_encode($res));
+    }
+  }
 });
 
-$server->on('message', function($server, $frame) {
-    echo "received message: {$frame->data}\n";
-    $data = json_decode($frame->data);
-
-    if ($data->msg === "START"){
-        $res = [
-            "type" => "INIT_GAME", 
-            "gameType" => "PASKAHOUSU", 
-            "players" => iterator_count($server->connections)];
-        $server->push($frame->fd, json_encode($res));
-  } 
-});
-
-$server->on('close', function($server, $fd) {
-    echo "connection close: {$fd}\n";
+$server->on('close', function ($server, $fd) {
+  echo "connection close: {$fd}\n";
 });
 
 $server->start();
